ed103c5ce603fe32e5a4e780f88033a811d895b4
Daniel Petrikovic <nuggetofgeraltia@gmail.com>
2024-09-12 10:24:12 +0200

    uvodny commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  return (
+  <Typography variant="h1" gutterBottom>
+    Hello my friend
+  </Typography>);
+}
