70bfbd76d8eacbee8890ceec0518cb819612457e
SpySauce121 <nuggetofgeraltia@gmail.com>
2025-01-08 09:06:02 +0100

    a

--- src/app/(public)/o-nas/page.tsx
+++ src/app/(public)/o-nas/page.tsx
@@ -3,7 +3,7 @@
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
-export const metadata = { title: "O mne | NaPrdAplikacia" }
+export const metadata = { title: "O mnes | NaPrdAplikacia" }
 
 export default function About() {
 
